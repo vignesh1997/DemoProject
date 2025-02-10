9290f253bbbecff8c06b3e609555992471cd5067
vignesh1997 <100225812+vignesh1997@users.noreply.github.com>
2025-02-10 21:52:02 +0530

    Update Array.java
    removed

--- src/SimpleProgram/Array.java
+++ src/SimpleProgram/Array.java
@@ -4,15 +4,7 @@ public class Array {
 
 	public static void main(String[] args) {
 	
-		int arr[]= {1,2,3,4,5,6,7};
-		int a=10;
-		int t=2;
-
-		System.out.println(t);
-
-		int r=a*t;
-		System.out.println(r);
-		//My name is vignesh, sakthi.
+		
 
 	}
 
